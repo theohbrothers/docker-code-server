d58c8a2ba572753d23f25fb8a538d3b5b748238e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-12-27 22:48:10 +0000

    Feature: Add `docker` variants
    - `docker` - Rooted `docker`
    - `docker-rootless` - Rootless `docker` can be run as a user.

---
+++ generate/templates/docker-entrypoint.sh.ps1
@@ -0,0 +1,30 @@
+@'
+#!/bin/sh
+set -eu
+
+'@
+
+if ($VARIANT['_metadata']['base_tag']) {
+    # Incremental build
+    foreach ($c in $VARIANT['_metadata']['components']) {
+        if ($c -eq 'docker') {
+@'
+echo "Starting dockerd"
+sudo dockerd &
+
+'@
+        }
+        if ($c -eq 'docker-rootless') {
+@'
+echo "Starting rootless dockerd"
+PATH=/home/user/bin:/sbin:/usr/sbin:$PATH dockerd-rootless.sh &
+
+'@
+        }
+    }
+}
+
+@'
+echo "Starting code-server"
+exec code-server --bind-addr 0.0.0.0:8080 --disable-telemetry --disable-update-check
+'@
