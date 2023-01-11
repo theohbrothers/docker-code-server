0aa8b9ce6069498d27726c6c1b49eca8991eedf4
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-01-11 01:17:38 +0000

    Enhancement: Install `docker` `20.10.22` in `docker` and `docker-rootless` variants
    Using `apk` to install `docker` does not allow installing specific version of packages. Now, `docker` is manually installed.

--- generate/templates/docker-entrypoint.sh.ps1
+++ generate/templates/docker-entrypoint.sh.ps1
@@ -17,8 +17,20 @@ sudo dockerd &
         }
         if ($c -eq 'docker-rootless') {
 @'
+# Start rootless docker
+# See: https://github.com/moby/moby/blob/v20.10.22/contrib/dockerd-rootless.sh
+# See: https://github.com/docker-library/docker/blob/master/20.10/dind/dockerd-entrypoint.sh
 echo "Starting rootless dockerd"
-PATH=/home/user/bin:/sbin:/usr/sbin:$PATH dockerd-rootless.sh &
+rootlesskit \
+    --net="${DOCKERD_ROOTLESS_ROOTLESSKIT_NET:-vpnkit}" \
+    --mtu="${DOCKERD_ROOTLESS_ROOTLESSKIT_MTU:-1500}" \
+    --disable-host-loopback \
+    --port-driver="${DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER:-builtin}" \
+    --copy-up=/etc \
+    --copy-up=/run \
+    --propagation=rslave \
+    ${DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS:-} \
+    dockerd &
 
 '@
         }
