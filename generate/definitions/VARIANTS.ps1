4061e6cd8174417d2e8fe2275c052dbba83f65ec
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-27 08:43:36 +0000

    Change: Remove `v` prefix from docker tags
    Docker image tag conventions are starting to become clear that it is preferred to omit `v` prefix. While having the `v` prefix in the past might have been common, it is becoming less and less common today, with most of docker official images
    (https://github.com/docker-library/official-images) omitting the `v` prefix.

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -37,9 +37,9 @@ $VARIANTS = @(
                     components = $subVariant['components']
                     job_group_key = $variant['package_version']
                 }
-                # Docker image tag. E.g. 'v2.17.0[-component..]'
+                # Docker image tag. E.g. '2.17.0' or '2.17.0[-component..]'
                 tag = @(
-                        "v$( $variant['package_version'] )"
+                        $variant['package_version']
                         $subVariant['components'] | ? { $_ }
                 ) -join '-'
                 tag_as_latest = if ($variant['package_version'] -eq $local:VARIANTS_MATRIX[0]['package_version'] -and $subVariant['components'].Count -eq 0) { $true } else { $false }
